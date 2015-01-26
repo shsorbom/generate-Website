670d0dba49d52f09a83399575e625ff70a220bb7
Shawn Sörbom <shawn@sorbom.com>
2015-01-25 16:00:26 -0800

    * Correctly generate links for new pages

--- subFunctions.sh
+++ subFunctions.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 function seek-pages
 {
+  # Ugly hack, sed was not cooperating
+  #------------------------------------
+  HostPageLink=`cat $1|grep NavLinks|grep strong`
+  echo $HostPageLink >temp.txt
+  # using "@" as delimeter for sed
+  sed -i "s@#@$1@" temp.txt
+  sed -i "s@<strong>@""@" temp.txt
+  sed -i "s@</strong>@""@" temp.txt
+  HostPageLink=`cat temp.txt`
+  rm temp.txt
+  #-----------------------------------
   IFS=$'\n'       # make newlines the only separator
       #echo "Files: `ls -1 |grep NavLinks $1`" # debug
+      
   for links in `ls -1 |grep NavLinks $1`
   do
   {
+    if [ `echo $links| grep strong` != "" ]
+    then
     sed -i "/position=\"endOfNav\"/i \
-    $links"
+    $HostPageLink" "$fileName"
+    continue
+    fi
+    sed -i "/position=\"endOfNav\"/i \
+    $links" "$fileName"
   }
 done
-HostPageLink=`cat $1|grep NavLinks|grep strong`
-# Ugly hack, sed was not cooperating
-#------------------------------------
-echo $HostPageLink >temp.txt
-# using "@" as delimeter for sed
-sed -i "s@#@$1@" temp.txt
-sed -i "s@<strong>@""@" temp.txt
-sed -i "s@</strong>@""@" temp.txt
-HostPageLink=`cat temp.txt`
-rm temp.txt
-#-----------------------------------
-echo new link: 
-echo $HostPageLink
 }
